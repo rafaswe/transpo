f2079087f198308d23cb031f96b4778d05143043
rafaswe <mahiya35-2706@diu.edu.bd>
2021-12-03 00:18:02 +0600

    Use token

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAX6GfF9bQRZwNYUTbQaPOBbihJqxHPaoI",
-    authDomain: "react-transpo.firebaseapp.com",
-    projectId: "react-transpo",
-    storageBucket: "react-transpo.appspot.com",
-    messagingSenderId: "206865447303",
-    appId: "1:206865447303:web:000ac51154fddc15807b22"
+    apiKey: "Use your own",
+    authDomain: "Use your own",
+    projectId: "Use your own",
+    storageBucket: "Use your own",
+    messagingSenderId: "Use your own",
+    appId: "Use your own"
 };
 export default firebaseConfig;
